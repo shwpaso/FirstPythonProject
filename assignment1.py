a5ed12ae104585cc144948f47ddc7f02b569b308
shwepaso <shweta.bhaskara31@gmail.com>
2017-12-12 15:24:32 -0800

    myfirstassignment

---
+++ assignment1.py
@@ -0,0 +1,135 @@
+
+
+
+
+str1="pavan santosh"
+
+print("length of str1 is ",len(str1))
+
+
+
+""""
+
+a=10
+
+b=20
+
+c="redhat"
+
+
+
+print(a,b,c,sep=" ")
+
+print(a,b,c,end=" ")
+
+"""
+
+
+
+"""
+
+c="string1"
+
+d="abc"
+
+
+
+
+
+print("type is string{} and value is {}".format(c,d))
+
+"""
+
+#character frequency in a string
+
+str2="pavan santosh"
+
+print(str2.count("a"))
+
+
+
+
+
+#reverse a string
+
+print(reversed("pavan santosh"))
+
+
+
+
+
+
+
+#repeated chars in a string
+
+str4="pavan santosh"
+
+print(str4.count("a",3,13))
+
+
+
+
+
+#index of a character
+
+str5="python class"
+
+print("index of o is",str5.find("o"))
+
+
+
+str_5="python programming"
+
+print(len(str_5))
+
+print(str_5.index("o"))
+
+print(str_5.rindex("o"))
+
+
+
+#to find if all character are alphabets
+
+str6="python"
+
+str_6="python "
+
+str_06="python001"
+
+
+
+print(str6.isalpha())
+
+print(str_6.isalpha())
+
+print(str_06.isalpha())
+
+
+
+
+
+#7 string to list
+
+
+
+str7="1 2 3"
+
+print(str7.split())
+
+
+
+str_7="cars toys bikes"
+
+print(str_7.split())
+
+str9="python is a godd programming language"
+print(str9.rpartition(" "))
+
+str10="pyTHoN"
+print(str10.swapcase())
+print(str10.lower())
+print(str10.upper())
+
+
+
+
