45feb098e4ea3ea2b9c1efc1dd4b58957215b141
shwepaso <shweta.bhaskara31@gmail.com>
2017-12-19 03:35:07 -0800

    forloops

---
+++ conditionstatements/for1.py
@@ -0,0 +1,25 @@
+count=0
+
+
+
+name=input("enter name")
+
+
+
+for letter in name :
+
+    if (letter in ["a","e","i","o","u","A","E","I","O","U"]):
+
+        count=count+1
+
+
+
+
+
+print("your name contains",count,"vowels")
+
+
+
+#print("your name contains {} vowels".format(count))
+
+#.format method
