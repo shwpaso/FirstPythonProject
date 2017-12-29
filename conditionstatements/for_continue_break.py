e140c5d9608761034f6c3e1f547adeacad25e11c
shwepaso <shweta.bhaskara31@gmail.com>
2017-12-28 18:40:48 -0800

    conditionstatements

---
+++ conditionstatements/for_continue_break.py
@@ -0,0 +1,17 @@
+name=input("enter name")
+
+
+
+for letter in name:
+
+
+
+    if (letter==" "):
+
+        break  #program terminates as soon as space is ecountered
+
+     #continue  # if we give continue whenever space is encountered it will continue
+
+    else:
+
+        print(letter)
